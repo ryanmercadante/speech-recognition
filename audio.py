7707ad65f65f4058cbe811e99622b1ccf71cc4e1
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2018-11-02 11:06:51 -0400

    function to play audio

---
+++ audio.py
@@ -0,0 +1,25 @@
+import pyaudio
+import wave
+
+def play_audio(filename):
+    chunk = 1024
+    wf = wave.open(filename, 'rb')
+    pa = pyaudio.PyAudio()
+
+    stream = pa.open(
+        format = pa.get_format_from_width(wf.getsampwidth()),
+        channels = wf.getnchannels(),
+        rate = wf.getframerate(),
+        output = True
+    )
+
+    data_stream = wf.readframes(chunk)
+
+    while data_stream:
+        stream.write(data_stream)
+        data_stream = wf.readframes(chunk)
+
+    stream.close()
+    pa.terminate()
+
+play_audio('./audio/wet.wav')
